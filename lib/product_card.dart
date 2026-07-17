172fe7f8c70756f17028bf5199324a050c151dac
abrarkhan02ak <abrarytking@gmail.com>
2026-07-17 20:41:37 +0530

    Create lib/product_card.dart

---
+++ lib/product_card.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final String code;
+  final String name;
+  final String price;
+
+  const ProductCard({
+    super.key,
+    required this.code,
+    required this.name,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      child: ListTile(
+        leading: const Icon(Icons.shopping_bag),
+        title: Text(name),
+        subtitle: Text(
+          "Product Code: $code\nWholesale Price: ₹$price",
+        ),
+        trailing: ElevatedButton(
+          onPressed: () {},
+          child: const Text("Add"),
+        ),
+      ),
+    );
+  }
+}
